fb4681b2bb6812f65fddf7adb0c2f156ef25f29e
J511Y <47891005+J511Y@users.noreply.github.com>
2020-02-09 20:13:24 +0900

    [이도영][프로그래머스_두 정수 사이의 합][일반적인 방법]

--- coding_test/coding_test/src/이도영/solution_v1.java
+++ coding_test/coding_test/src/이도영/solution_v1.java
@@ -1,5 +1,11 @@
 package 이도영;
-
+/*
+ * 유준혁 멘토 덧댐 2020.02.09 20:10
+ * ---------------------------
+ * 일반적인 방법.
+ * 코드의 양을 줄이기 위해 조건문 안에 바로 값을 집어넣은 걸로 보임.
+ * 다만 조건을 계산할때 어떤식으로 이루어지는지 한 번 확인 필요.
+ */
 public class solution_v1 {
 	public static void main(String[] args) {
 		System.out.println(solution(3, 5));
