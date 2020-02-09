d92c10adb62ebe9a77dc906c6b8de1d7825a1269
J511Y <47891005+J511Y@users.noreply.github.com>
2020-02-09 20:14:52 +0900

    [박민진][프로그래머스_두 정수 사이의 합][일반적인 방법]

--- coding_test/coding_test/src/박민진/Sumex1.java
+++ coding_test/coding_test/src/박민진/Sumex1.java
@@ -1,5 +1,10 @@
 package 박민진;
-
+/*
+ * 유준혁 멘토 덧댐 2020.02.09 20:14
+ * ---------------------------
+ * 일반적인 방법.
+ * 대소 구분이 없이 주어지므로 3가지의 예외처리를 하여 return하는 방식.
+ */
 public class Sumex1 {
 	static long solution(int a, int b) {
 		long answer = 0;
