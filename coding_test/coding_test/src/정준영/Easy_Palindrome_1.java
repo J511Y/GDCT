ccd7f8ae92dbab8c1f82f874e9b04f3c1d742357
J511Y <47891005+J511Y@users.noreply.github.com>
2020-02-09 20:24:21 +0900

    [정준영][백준_팰린드롬인지 확인하기_10988][일반적인 방법_boolean flag 사용]

--- coding_test/coding_test/src/정준영/Easy_Palindrome_1.java
+++ coding_test/coding_test/src/정준영/Easy_Palindrome_1.java
@@ -1,5 +1,12 @@
 package 정준영;
-
+/*
+ * 유준혁 멘토 덧댐 2020.02.09 20:22
+ * ---------------------------
+ * 일반적인 방법.
+ * 입력받은 문자열의 양쪽 끝에서부터 하나씩 확인해보는 방식.
+ * check 라는 boolean을 두고 틀린걸 발견했을 시 false로 만듦.
+ * 마지막으로 해당 boolean을 참조하여 결과값 출력.
+ */
 import java.util.Scanner;
 
 public class Easy_Palindrome_1 {
