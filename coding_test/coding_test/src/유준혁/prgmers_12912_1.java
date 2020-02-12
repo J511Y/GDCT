d4b4793ceaef45ff182328a192a2b49ba5481eb4
J511Y <47891005+J511Y@users.noreply.github.com>
2020-02-12 22:29:33 +0900

    [유준혁][프로그래머스_두 정수 사이의 합][재귀 + 모든 경우의 수 처리]

--- coding_test/coding_test/src/유준혁/prgmers_12912_1.java
+++ coding_test/coding_test/src/유준혁/prgmers_12912_1.java
@@ -3,7 +3,7 @@ package 유준혁;
  * https://programmers.co.kr/learn/courses/30/lessons/12912
  * 두 정수 사이의 합
  * ----------------
- * 재귀를 사용한 방법
+ * 재귀를 사용한 방법 + 3가지 경우 처리하는 일반적인 방법
  */
 public class prgmers_12912_1 {
 	static private long solution(int a, int b) {
