ed1ae57a2b9aa415a1eade690dd812b47e3e7ab4
J511Y <rpdla1456@gmail.com>
2020-02-14 16:54:01 +0900

    [유준혁][2020_쿠팡_3번문제][풀이완료]

--- coding_test/coding_test/src/유준혁/coupang_1.java
+++ coding_test/coding_test/src/유준혁/coupang_1.java
@@ -1,25 +1,37 @@
 package 유준혁;
-// 풀다말았음
 import java.util.*;
 public class coupang_1 {
 	static int len, max;
 	static int[] vst;
 	static int[][] del;
 	static int[] w = new int[] {1, -1, len, -len};
+	static List<Integer> list = new ArrayList<>();
 	
 	static void dfs(int x, int sum, int time) {
+		/// 출력을 위한 list
+		list.add(x);
+		vst[x] = 1;
 		if(time == len * len) return;
 		for(int i = 0; i < 4; i++) {
 			int a = x + w[i];
+			if(i < 2 && (a / len != x / len)) continue;
 			if(0 <= a && a < (len * len)) {
 				int tmpSum = sum;
 				boolean already = vst[a] == 1;
 				if(del[a][0] > time && vst[a] == 0) {
 					tmpSum += del[a][1];
 					max = Math.max(max, tmpSum);
+					
+					/// 출력문
+					list.add(a);
+					System.out.println(list + "\n=>" + tmpSum + "\n");
+					list.remove(list.size() - 1);
 				}
-				vst[a] = 1;
 				dfs(a, tmpSum, time + 1);
+				
+				/// 출력문
+				list.remove(list.size() - 1);
+				
 				vst[a] = already ? 1 : 0;
 			}
 		}
@@ -29,7 +41,6 @@ public class coupang_1 {
 		vst = new int[r * r];
 		len = r;
 		w[2] = len; w[3] = -len;
-		vst[0] = 1;
 		max = delivery[0][1];
 		dfs(0, delivery[0][1], 0);
 		return max;
