887e4bc7511da62d05dc7228aa4e22499589f207
Do <Do@DESKTOP-R8JSCGA>
2020-02-27 22:32:13 +0900

    [이도영][백준_2164_카드2]

---
+++ coding_test/coding_test/src/이도영/baekjoon_2164.java
@@ -0,0 +1,20 @@
+package ¿Ãµµøµ;
+import java.io.*;
+import java.util.*;
+
+public class baekjoon_2164 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<Integer>();
+		for (int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		while (q.size() != 1) {
+			q.poll();
+			q.add(q.poll());
+		}
+		System.out.println(q.poll() + "\n");
+	}
+}
